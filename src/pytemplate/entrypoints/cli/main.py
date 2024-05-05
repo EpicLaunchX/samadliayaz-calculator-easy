d3e9ca0f5b69cbd2a2963317a299351926064f11
Ayaz Samadli <samadlia@ayazs-air.speedport.ip>
2024-05-05 12:27:57 +0200

    fix: blacked

--- src/pytemplate/entrypoints/cli/main.py
+++ src/pytemplate/entrypoints/cli/main.py
@@ -7,7 +7,7 @@ from pytemplate.service.calculator import Calculator
 def main() -> Optional[int]:
     calculator = Calculator()
     try:
-        op1, op2, op_name = input().split(',')
+        op1, op2, op_name = input().split(",")
         operand = getattr(calculator, op_name.strip(), None)
 
         op1 = int(op1)
@@ -16,12 +16,11 @@ def main() -> Optional[int]:
             raise ValueError(f"Unsupported operand type: {op_name}")
     except ValueError:
         print(
-            "Please enter two integer and one string operand (add, subtract, "
-            "multiply or divide) by comma seperated. e.g.: 45, 35, add. "
+            "Please enter two integer and one string operand (add, subtract, " "multiply or divide) by comma seperated. e.g.: 45, 35, add. "
         )
         return None
     return operand(operands_factory(op1, op2))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,16 +5,19 @@ import pytest
 from src.pytemplate import main
 
 
-@pytest.mark.parametrize("input, output", [
-    ("45, 35, add", 80),
-    ("45, 35, subtract", 10),
-    ("3, 5, multiply", 15),
-    ("5, 2, divide", 2),
-    ("1 2 add", None),
-    ("one, 2, add", None),
-    ("1, 2, mult", None)
-])
-def test_main_success(input, output):
-    with patch('builtins.input', return_value=input):
+@pytest.mark.parametrize(
+    "input, output",
+    [
+        ("45, 35, add", 80),
+        ("45, 35, subtract", 10),
+        ("3, 5, multiply", 15),
+        ("5, 2, divide", 2),
+        ("1 2 add", None),
+        ("one, 2, add", None),
+        ("1, 2, mult", None),
+    ],
+)
+def test_main(input, output):
+    with patch("builtins.input", return_value=input):
         result = main()
         assert result == output
