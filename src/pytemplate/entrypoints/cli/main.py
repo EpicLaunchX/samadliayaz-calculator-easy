9bbcf93776010e40e631a21a3168ff29fe1dd503
Ayaz Samadli <samadlia@ayazs-air.speedport.ip>
2024-05-07 00:38:13 +0200

    fix: rename variables

--- src/pytemplate/entrypoints/cli/main.py
+++ src/pytemplate/entrypoints/cli/main.py
@@ -4,23 +4,23 @@ from pytemplate.domain.models import operands_factory
 from pytemplate.service.calculator import Calculator
 
 
-def main() -> Optional[int]:
+def main() -> None:
     calculator = Calculator()
     try:
-        op1, op2, op_name = input().split(",")
-        operand = getattr(calculator, op_name.strip(), None)
+        first_operand, second_operand, operand_name = input().split(",")
+        operand = getattr(calculator, operand_name.strip(), None)
 
-        op1 = int(op1)
-        op2 = int(op2)
+        first_operand = int(first_operand)
+        second_operand = int(second_operand)
         if operand is None:
-            raise ValueError(f"Unsupported operand type: {op_name}")
+            raise ValueError(f"Unsupported operand type: {operand_name}")
     except ValueError:
         print(
             "Please enter two integer and one string operand (add, subtract, " "multiply or divide) by comma seperated. e.g.: 45, 35, add."
         )
         return None
     else:
-        result = operand(operands_factory(op1, op2))
+        result = operand(operands_factory(first_operand, second_operand))
         print(result)
 
 
